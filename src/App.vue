1b4b059756842790fc7a5f9a2629d3547f2c6ea4
Theresa Ulwahn <theresaulwahn@gmail.com>
2021-06-06 17:05:40 +0200

    added tasks and component button

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container">
-  <Header />
+    <Header title="Task tracker"/>
   </div>
 
 </template>
@@ -12,6 +12,33 @@ export default {
   name: 'App',
   components: {
     Header
+  },
+  data() {
+    return {
+      tasks:[]
+    }
+  },
+  created() {
+    this.tasks = [
+      {
+        id: 1,
+        text: 'Doctors Appointment',
+        day: 'March 1st at 2:30',
+        reminder: true,
+      },
+      {
+        id: 2,
+        text: 'Meeting at School',
+        day: 'March 3st at 1:30',
+        reminder: true,
+      },
+      {
+        id: 3,
+        text: 'Food Shopping',
+        day: 'March 4st at 5:30',
+        reminder: false,
+      },
+    ]
   }
 }
 </script>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,16 +1,26 @@
 <template>
     <header>
         <h1>
-            Task Tracker
+            {{ title }}
         </h1>
+        <Button text="Add Task" color="green"/>
     </header>
 </template>
 
 <script>
+import Button from './Button'
+
     export default {
         name: 'Header',
+        props: {
+            title: String,
+        },
+        components: {
+            Button
+        }
     }
 </script>
+
 
 <style scoped>
 header {
